994322fe241c8b6501a6beea0f19737bed7fd203
linytsysu <linytsysu@163.com>
2019-10-04 13:28:29 +0800

    Update

---
+++ fine-tune/submission.py
@@ -0,0 +1,54 @@
+import numpy as np
+import pandas as pd
+import keras
+import codecs
+from keras_bert import Tokenizer, get_custom_objects
+
+test_df = pd.read_csv('/data/bert_finetune/data/Test_DataSet.csv')
+test_df = test_df.fillna('EMPTY')
+test_df['titlecontent'] = test_df['title'] + test_df['content']
+
+vocab_path = '/data/bert_finetune/bert_model/vocab.txt'
+token_dict = {}
+with codecs.open(vocab_path, 'r', 'utf8') as reader:
+    for line in reader:
+        token = line.strip()
+        token_dict[token] = len(token_dict)
+
+SEQ_LEN = 512
+model = keras.models.load_model('model2.h5', custom_objects=get_custom_objects())
+
+tokenizer = Tokenizer(token_dict)
+
+class OurTokenizer(Tokenizer):
+    def _tokenize(self, text):
+        R = []
+        for c in text:
+            if c in self._token_dict:
+                R.append(c)
+            elif self._is_space(c):
+                R.append('[unused1]') # space类用未经训练的[unused1]表示
+            else:
+                R.append('[UNK]') # 剩余的字符是[UNK]
+        return R
+
+tokenizer = OurTokenizer(token_dict)
+
+test_text = [text for text in test_df['titlecontent'].values]
+indices = []
+for text in test_text:
+    ids, segments = tokenizer.encode(text, max_len=SEQ_LEN)
+    indices.append(ids)
+indices = np.array(indices)
+X_test = [indices, np.zeros_like(indices)]
+
+result = model.predict(X_test)
+new_label = np.argmax(result, axis=1)
+
+new_id = test_df['id'].values
+
+submission_df = pd.DataFrame()
+submission_df['id'] = new_id
+submission_df['label'] = new_label
+submission_df.to_csv('submission.csv', index=None)
+
---
+++ fine-tune/train.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import pandas as pd
+
+content_df = pd.read_csv('/data/bert_finetune/data/Train_DataSet.csv')
+label_df = pd.read_csv('/data/bert_finetune/data/Train_DataSet_Label.csv')
+
+df = content_df.merge(label_df, on='id')
+
+print(df[df['label'] == 2].shape, df[df['label'] == 1].shape, df[df['label'] == 0].shape)
+
+df = df.fillna('EMPTY')
+
+df['titlecontent'] = df['title'] + df['content']
+
+config_path = '/data/bert_finetune/bert_model/bert_config.json'
+checkpoint_path = '/data/bert_finetune/bert_model/bert_model.ckpt'
+vocab_path = '/data/bert_finetune/bert_model/vocab.txt'
+
+import codecs
+from keras_bert import load_trained_model_from_checkpoint
+
+token_dict = {}
+with codecs.open(vocab_path, 'r', 'utf8') as reader:
+    for line in reader:
+        token = line.strip()
+        token_dict[token] = len(token_dict)
+
+SEQ_LEN = 512
+model = load_trained_model_from_checkpoint(
+    config_path,
+    checkpoint_path,
+    training=True,
+    trainable=True,
+    seq_len=SEQ_LEN,
+)
+
+import os
+import numpy as np
+from tqdm import tqdm
+from keras_bert import Tokenizer
+
+tokenizer = Tokenizer(token_dict)
+
+
+from sklearn.model_selection import train_test_split
+
+class OurTokenizer(Tokenizer):
+    def _tokenize(self, text):
+        R = []
+        for c in text:
+            if c in self._token_dict:
+                R.append(c)
+            elif self._is_space(c):
+                R.append('[unused1]') # space类用未经训练的[unused1]表示
+            else:
+                R.append('[UNK]') # 剩余的字符是[UNK]
+        return R
+
+tokenizer = OurTokenizer(token_dict)
+
+
+BATCH_SIZE = 32
+indices, sentiments = [], []
+data = df[['titlecontent', 'label']].values
+for i in range(data.shape[0]):
+    text = data[i][0]
+    sentiment = data[i][1]
+    ids, segments = tokenizer.encode(text, max_len=SEQ_LEN)
+    indices.append(ids)
+    sentiments.append(sentiment)
+items = list(zip(indices, sentiments))
+indices, sentiments = zip(*items)
+indices = np.array(indices)
+mod = indices.shape[0] % BATCH_SIZE
+if mod > 0:
+    indices, sentiments = indices[:-mod], sentiments[:-mod]
+
+
+X = [indices, np.zeros_like(indices)]
+y = np.array(sentiments)
+
+
+LR = 1e-5
+import keras
+inputs = model.inputs[:2]
+dense = model.get_layer('NSP-Dense').output
+#extract = model.get_layer('Extract').output
+#average = keras.layers.average(inputs=[dense, extract])
+h1 = keras.layers.Dense(units=64, activation='relu')(dense)
+outputs = keras.layers.Dense(units=3, activation='softmax')(h1)
+model = keras.models.Model(inputs, outputs)
+model.compile(
+    keras.optimizers.Adam(lr=LR),
+    loss='categorical_crossentropy',
+    metrics=['accuracy'],
+)
+
+
+from sklearn.preprocessing import LabelEncoder
+encoder = LabelEncoder()
+y = encoder.fit_transform(y)
+y = keras.utils.np_utils.to_categorical(y)
+
+model.fit(
+    X,
+    y,
+    epochs=5,
+    batch_size=BATCH_SIZE,
+)
+
+model.save('/data/bert_finetune/model3.h5')
